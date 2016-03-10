90cb972d475a20cb975e25217cf73af7665bd594
vicalloy <zbirder@gmail.com>
2016-03-10 11:29:47 +0800

    remove unicode

--- lbutils/tests/models.py
+++ lbutils/tests/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils import six
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -9,22 +10,16 @@ from django.utils.encoding import python_2_unicode_compatible
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
-        return unicode(self.name)
-
     def __str__(self):
-        return self.name
+        return six.text_type(self.name)
 
 
 @python_2_unicode_compatible
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
-    def __unicode__(self):
-        return self.name
-
     def __str__(self):
-        return self.name
+        return six.text_type(self.name)
 
 
 @python_2_unicode_compatible
@@ -43,8 +38,5 @@ class Book(models.Model):
             ("sft_mgr_book", "manager book"),
         )
 
-    def __unicode__(self):
-        return self.name
-
     def __str__(self):
-        return self.name
+        return six.text_type(self.name)
