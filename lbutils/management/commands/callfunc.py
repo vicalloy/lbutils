3fafb209740664a9287a84f08c4f8c26e28c768d
vicalloy <zbirder@gmail.com>
2017-04-24 21:24:43 +0800

    add command callfunc

---
+++ lbutils/management/commands/callfunc.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+
+from lbutils import as_callable
+
+
+class Command(BaseCommand):
+    help = "Call function"
+
+    def add_arguments(self, parser):
+        parser.add_argument('func')
+
+    def handle(self, *args, **options):
+        func = options['func']
+        func = as_callable(func)
+        func()
